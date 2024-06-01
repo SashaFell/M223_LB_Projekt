b1db3511526909c391220aca27d5b51412777232
SashaFell <sasha.fellmann@bluewin.ch>
2024-06-02 01:38:14 +0200

    Added Axios

--- m223_frontend/src/login.js
+++ m223_frontend/src/login.js
@@ -1,7 +1,12 @@
 import React from 'react'
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from "./context/AuthProvider";
+
+import axios from './api/axios';
+const LOGIN_URL = '/auth';
 
 const login = () => {
+    const { setAuth } = useContext(AuthContext)
     const userRef = useRef();
     const errRef = useRef();
 
@@ -20,10 +25,35 @@ const login = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        comsole.log(user, pwd);
-        setUser('');
-        setPwd('');
-        setSuccess(true);
+
+        try {
+            const response = await axios.post(LOGIN_URL,
+                JSON.stringify({ user, pwd }),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+            console.log(JSON.stringify(response?.data));
+            //console.log(JSON.stringify(response));
+            const accessToken = response?.data?.accessToken;
+            const roles = response?.data?.roles;
+            setAuth({ user, pwd, roles, accessToken });
+            setUser('');
+            setPwd('');
+            setSuccess(true);
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 400) {
+                setErrMsg('Missing Username or Password');
+            } else if (err.response?.status === 401) {
+                setErrMsg('Unauthorized');
+            } else {
+                setErrMsg('Login Failed');
+            }
+            errRef.current.focus();
+        }
     }
   return (
     <>
